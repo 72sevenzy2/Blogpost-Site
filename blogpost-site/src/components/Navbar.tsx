1d2f53888f4df41dd4393edded883196a363e09b
72 <72sevenzy2@gmail.com>
2025-10-07 07:02:06 +0500

    quick changes

--- blogpost-site/src/components/Navbar.tsx
+++ blogpost-site/src/components/Navbar.tsx
@@ -1,22 +1,20 @@
- 
 import { assets } from '../assets/assets';
+import type { JSX } from 'react';
 
- const Navbar = () => {
-
-   
-    return (
+const Navbar = (): JSX.Element => {
+  return (
     <div className='flex justify-between items-center py-5 mx-8 sm:ms-20 xl:mx-32'>
-      <img  src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' />
-       
-        <button
-            className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
-        >
-          Dashboard
-          <img src={assets.arrow} className="w-3" alt="arrow" />
-       
-        </button>
-    
+      <img src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' />
+
+      <button
+        className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+      >
+        Dashboard
+        <img src={assets.arrow} className="w-3" alt="arrow" />
+
+      </button>
+
     </div>
   )
 }
- export default Navbar
+export default Navbar
